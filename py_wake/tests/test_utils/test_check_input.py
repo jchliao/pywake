489047986c228b1f58a14848d11c45508b19c3d5
Mads M. Pedersen <mmpe@dtu.dk>
2025-05-27 11:58:40 +0200

    add ptf

--- py_wake/tests/test_utils/test_check_input.py
+++ py_wake/tests/test_utils/test_check_input.py
@@ -1,6 +1,8 @@
 import pytest
 from py_wake.utils.check_input import check_input
 from py_wake import np
+from pathlib import Path
+from py_wake.tests import ptf
 
 
 def test_check_input():
@@ -16,3 +18,8 @@ def test_check_input():
         check_input(input_space, np.array([(1, 250)]).T, ['ws', 'wd'])
 
     check_input(input_space, np.array([(1, 200)]).T, ['ws', 'wd'])
+
+
+def test_ptf():
+    assert Path(ptf('test.txt',
+                    'ecd71870d1963316a97e3ac3408c9835ad8cf0f3c1bc703527c30265534f75ae')).read_text() == 'test123'
